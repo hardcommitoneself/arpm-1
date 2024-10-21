0cbef253c9c5fb40e998d3f4d7b5384bd67409ab
hardcommitoneself <hardcommitoneself@gmail.com>
2024-10-21 18:35:13 +0700

    all done

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CumulativeSumController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/upload-data', [CumulativeSumController::class, 'generateAndUploadData']);
